2330a666533c9f83c9f80410fc83af2ab000e001
fogelito <fogelshmuel@gmail.com>
2022-05-12 19:57:05 +0300

    crud

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,44 @@
 FROM phpswoole/swoole:4.6-php7.4-alpine
-
+FROM composer:2.0 as composer
 WORKDIR /app
 
-COPY composer.lock /app
+ENV PHP_SWOOLE_VERSION="v4.8.3"
+
 COPY composer.json /app
 
-RUN composer install
+RUN composer install --ignore-platform-reqs --optimize-autoloader --no-plugins --no-scripts --prefer-dist
+
+RUN \
+  apk add --no-cache --virtual .deps \
+  make \
+  automake \
+  autoconf \
+  gcc \
+  g++ \
+  git \
+  zlib-dev \
+  brotli-dev \
+  openssl-dev \
+  yaml-dev
+
+RUN git clone --depth 1 --branch "${PHP_SWOOLE_VERSION}" https://github.com/swoole/swoole-src.git && \
+  cd swoole-src && \
+  phpize && \
+  ./configure --enable-http2 && \
+  make && make install && \
+  cd ..
+
+RUN docker-php-ext-install \
+    pdo \
+    pdo_mysql
+
+RUN pecl install redis
+
+RUN docker-php-ext-enable \
+    swoole \
+    pdo \
+    pdo_mysql \
+    redis
 
 COPY . /app
 
--- app/server.php
+++ app/server.php
@@ -5,6 +5,12 @@ if (file_exists(__DIR__.'/../vendor/autoload.php')) {
 }
 
 use Utopia\App;
+use Utopia\Cache\Adapter\None;
+use Utopia\Cache\Adapter\Redis as RedisCache;
+use Utopia\Cache\Cache;
+use Utopia\Database\Adapter\MariaDB;
+use Utopia\Database\Database;
+use Utopia\Registry\Registry;
 use Utopia\Swoole\Request;
 use Utopia\Swoole\Response;
 use Utopia\Swoole\Files;
@@ -41,10 +47,19 @@ App::init(function($response) {
         ->addHeader('X-XSS-Protection', '1;mode=block');
 }, ['response'], '*');
 
+
 App::shutdown(function($request) {
     $date = new DateTime();
     Console::success($date->format('c').' '.$request->getURI());
 }, ['request'], 'api');
+
+
+App::error(function(Exception $error) {
+
+    var_dump($error->getCode());
+    var_dump($error->getLine());
+    var_dump($error->getMessage());
+}, ['error'], 'api');
 
 /*
     The routes are defined before the Swoole server is turned on.
@@ -63,15 +78,40 @@ App::get('/')
         }
     );
 
+
+App::get('/create-collection')
+    ->groups(['api'])
+    ->inject('request')
+    ->inject('response')
+    ->inject('db')
+    ->action(
+        function($request, $response, Database $db) {
+            $collection = $db->getCollection('tasks');
+
+            if($collection->isEmpty()){
+                $db->createAttribute('tasks', 'title', Database::VAR_STRING, 1000000, true);
+                $db->createAttribute('tasks', 'time', Database::VAR_INTEGER, 0, true);
+                $db->createAttribute('tasks', 'is_active', Database::VAR_BOOLEAN, 0, true);
+                $db->createAttribute('tasks', 'string_list', Database::VAR_STRING, 0, true, null, true, true);
+                $collection = $db->getCollection('tasks');
+            }
+
+             $response->json([$collection]);
+
+        }
+    );
+
+
 App::get('/hello')
     ->groups(['api'])
     ->inject('request')
     ->inject('response')
     ->action(
         function($request, $response) {
-            $response->json(['Hello' => 'World']);
+            $response->json(['Hello' => 'World4']);
         }
     );
+
 
 App::get('/goodbye')
     ->groups(['api'])
@@ -79,7 +119,7 @@ App::get('/goodbye')
     ->inject('response')
     ->action(
         function($request, $response) {
-            $response->json(['Goodbye' => 'World']);
+            $response->json(['Goodbye' => 'World1']);
         }
     );
 
@@ -87,11 +127,49 @@ App::get('/goodbye')
     Configure the Swoole server to respond with the Utopia app.    
 */
 
-$http->on('request', function (SwooleRequest $swooleRequest, SwooleResponse $swooleResponse) {
+$registry = new Registry;
 
+$registry->set('db', function () { // This is usually for our workers or CLI commands scope
+    $dbHost = 'mariadb';
+    $dbPort = '3306';
+    $dbUser = 'root';
+    $dbPass = 'password';
+    $dbScheme = 'mydb';
+
+    $pdo = new PDO("mysql:host={$dbHost};port={$dbPort};dbname={$dbScheme};charset=utf8mb4", $dbUser, $dbPass, array(
+        PDO::ATTR_TIMEOUT => 3, // Seconds
+        PDO::ATTR_PERSISTENT => true,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ));
+
+    $dbScheme = 'shimo';
+
+    $cache = new Cache(new None());
+    $database = new Database(new MariaDB($pdo), $cache);
+    $database->setNamespace('ns');
+    $database->exists($dbScheme) || $database->create($dbScheme);
+    $database->setDefaultDatabase($dbScheme);
+
+    return $database;
+
+});
+
+
+App::setResource('registry', function () use ($registry) {
+    return  $registry;
+});
+
+
+App::setResource('db', function ($registry) {
+    return  $registry->get('db');
+}, ['registry']);
+
+
+$http->on('request', function (SwooleRequest $swooleRequest, SwooleResponse $swooleResponse) {
     $request = new Request($swooleRequest);
     $response = new Response($swooleResponse);
-    $app = new App('America/Toronto');
+    $app = new App('Asia/Jerusalem');
 
     try {
         $app->run($request, $response);
